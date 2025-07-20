628adaf7b87142dadd724cac599d07fdc077bb9f
RaghuSharma14 <raghusharma70876@gmail.com>
2025-07-20 13:03:39 +0530

    Fix: Add searchTerm prop to NewsFeed for Vercel build

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <Layout>
       <SettingsPanel />
-      <NewsFeed />
+      <NewsFeed searchTerm="" />
       <Recommendations />
     </Layout>
   )
